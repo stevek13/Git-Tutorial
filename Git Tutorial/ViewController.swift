3f9a926d9725ce7a029f7c25ca46d306efee125a
Steve Kite <skite1@hotmail.com>
2018-10-24 11:50:31 -0700

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Steve on 10/24/18.
+//  Copyright © 2018 Steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
